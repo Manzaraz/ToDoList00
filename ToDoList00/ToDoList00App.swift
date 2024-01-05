666fb1a336e78057e3581f33db5dd165bcf24a11
Christian Manzaraz <magnusmanz@gmail.com>
2024-01-05 18:19:45 -0300

    Initial Commit

---
+++ ToDoList00/ToDoList00App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList00App.swift
+//  ToDoList00
+//
+//  Created by Christian Manzaraz on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList00App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
